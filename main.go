9216c18520b14f740ea29e25e5540eee23302606
panscan <joeavanzato@gmail.com>
2025-03-29 22:25:27 -0400

    Improving argument documentation, logic and adding some additional Asym related keys and staging new ones

--- main.go
+++ main.go
@@ -42,11 +42,6 @@ var subdir_names = []string{"finance", "reports", "documents", "it", "exhibits",
 var file_names = []string{"workbook", "report", "export", "timesheet", "evidence", "knowledge", "system", "adjustment", "observable", "genuine", "certificates", "judgement", "corporate", "domain", "tempest", "research", "hypothesis", "parallel"}
 var subFileNames = []string{"base", "employee", "confidential", "sensitive", "markings", "delimited", "full", "temporary", "interim", "remarkable", "explicit", "absolute", "aws", "directreport", "hr", "it", "fin", "monthly", "quarterly", "daily"}
 
-func printFormattedMessage(msg string, level LogLevel) {
-	ts := time.Now().Format("2006-01-02T15:04:05.000Z")
-	fmt.Printf("%s|%s| %s\n", ts, level, msg)
-}
-
 func parseArgs(groups []RansomActor) (map[string]any, error) {
 	// Target Options
 	targetDirectory := flag.String("directory", "", "Target Directory - can be UNC Path (\\\\localhost\\C$\\test) or Local (C:\\test)")
@@ -55,13 +50,24 @@ func parseArgs(groups []RansomActor) (map[string]any, error) {
 	group := flag.String("group", "", "Specify a group to emulate - if none selected, will select one at random")
 	list := flag.Bool("list", false, "List available groups to emulate")
 	skip := flag.Bool("skipconfirm", false, "Skip Directory Confirmation Prompt (be careful!)")
-	workers := flag.Int("workers", 25, "How many goroutines to use for encryption")
 
 	// Encryption/Decryption Parameters - will override group settings if used for encryption/decryption
-	rsa_public := flag.String("rsa_public", "", "Specify RSA Public-Key File - if none, will generate and write to file")
-	rsa_private := flag.String("rsa_private", "", "Specify RSA Private-Key File - if none, will generate and write to file")
 	decrypt := flag.Bool("decrypt", false, "Attempt to decrypt using specified options - must include RSA Private Key and Group Name OR Cipher Used")
 	sym_cipher := flag.String("cipher", "", "Specify Symmetric Cipher for Encryption/Decryption")
+	encryption_percent := flag.Int("ep", 25, "What percentage of files over the max default 100% threshold to encrypt")
+	threshold_auto_fullencrypt := flag.Int64("threshold", 1024, "File size in bytes to automatically encrypt 100% of the contents if file Size < THIS")
+	workers := flag.Int("workers", 25, "How many goroutines to use for encryption")
+
+	// Offensive
+	killprocs := flag.Bool("killprocs", false, "Attempt to stop configured list of process binaries on the target machine prior to encryption")
+	vss := flag.Bool("vss", false, "Attempt to remove all VSS copies on the target host prior to encryption")
+
+	// Asymmetric Keys
+	generate_keys := flag.Bool("generate_keys", false, "If specified, will generate new RSA/ECC keys to use for encryption/decryption purposes")
+	rsa_public := flag.String("rsa_public", "", "Specify RSA Public-Key File to use - if blank, will use embedded key")
+	rsa_private := flag.String("rsa_private", "", "Specify RSA Private-Key File - must be specified with decrypt if asymmetric system is RSA")
+	ecc_public := flag.String("ecc_public", "", "Specify ECC Public-Key File to use - if blank, will use embedded key")
+	ecc_private := flag.String("ecc_private", "", "Specify ECC Private-Key File - must be specified with decrypt if asymmetric system is ECC")
 
 	// Dummy Data Creation Parameters
 	create := flag.Bool("create", false, "Create a mixture of dummy-data files in the target directory for encryption targeting - when using this, only files created by impact will be targeted for encryption, regardless of existence")
@@ -69,15 +75,19 @@ func parseArgs(groups []RansomActor) (map[string]any, error) {
 	create_size := flag.Int("create_size", 5000, "Size in megabytes of dummy-file data to target - distributed evenly across create_files count")
 	flag.Parse()
 
-	if *targetDirectory == "" && !*list {
-		return nil, errors.New("Empty Target Directory")
+	// If we are not listing threat actor templates or ge
+	if *targetDirectory == "" && !*list && !*generate_keys {
+		return nil, errors.New("Empty Target Directory - must specify for encryption/decryption operations")
 	}
 
+	// Must use one or the other
+	// inline refers to modifying a file and writing the encrypted portions back to the same file
+	// outline refers to creating a new file and deleting the original
 	if *method != "inline" && *method != "outline" {
 		return nil, errors.New("Invalid Encryption Method - must be 'inline' or 'outline")
 	}
 
-	// validate group
+	// validate group selection or pick random - if decrypting, this is a bad idea.
 	selectedGroup := RansomActor{}
 	if *group == "" {
 		n := rand.Intn(len(groups))
@@ -97,42 +107,69 @@ func parseArgs(groups []RansomActor) (map[string]any, error) {
 		}
 	}
 
-	if *decrypt && *rsa_private == "" {
-		return nil, errors.New(fmt.Sprintf("Must specify an RSA Private Key file when using -decrypt"))
+	// User must specify the assymmetric cipher being used for decryption
+	if *decrypt && *rsa_private == "" && *ecc_private == "" {
+		return nil, errors.New(fmt.Sprintf("Must specify an RSA/ECC Private Key file when using -decrypt"))
 	}
+
+	// User must specify the symmetric cipher being used for decryption
 	if *decrypt && *sym_cipher == "" && *group == "" {
 		return nil, errors.New(fmt.Sprintf("Must specify group or cipher when using -decrypt"))
 	}
 
+	// User must specify what decryption mode we are using
+	if *rsa_private != "" && *ecc_private != "" {
+		return nil, errors.New("Cannot use rsa_private and ecc_private together - must choose only one for decryption")
+	}
+
+	// File Existence/Read Checks - if specified, we should be able to os stat them
 	if *rsa_private != "" {
 		err := doesOSEntryExist(*rsa_private)
 		if err != nil {
 			return nil, errors.New(fmt.Sprintf("Error with specified RSA Private Key File: %s", err.Error()))
 		}
 	}
-
 	if *rsa_public != "" {
 		err := doesOSEntryExist(*rsa_public)
 		if err != nil {
 			return nil, errors.New(fmt.Sprintf("Error with specified RSA Public Key File: %s", err.Error()))
 		}
 	}
+	if *ecc_private != "" {
+		err := doesOSEntryExist(*ecc_private)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Error with specified ECC Private Key File: %s", err.Error()))
+		}
+	}
+	if *ecc_public != "" {
+		err := doesOSEntryExist(*ecc_public)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("Error with specified ECC Public Key File: %s", err.Error()))
+		}
+	}
 
 	arguments := map[string]any{
-		"target":       *targetDirectory,
-		"method":       *method,
-		"group":        selectedGroup,
-		"list":         *list,
-		"skip":         *skip,
-		"recursive":    *recursive,
-		"create":       *create,
-		"create_count": *create_count,
-		"create_size":  *create_size,
-		"workers":      *workers,
-		"rsa_public":   *rsa_public,
-		"rsa_private":  *rsa_private,
-		"decrypt":      *decrypt,
-		"sym_cipher":   *sym_cipher,
+		"target":                     *targetDirectory,
+		"method":                     *method,
+		"group":                      selectedGroup,
+		"list":                       *list,
+		"skip":                       *skip,
+		"recursive":                  *recursive,
+		"create":                     *create,
+		"create_count":               *create_count,
+		"create_size":                *create_size,
+		"workers":                    *workers,
+		"rsa_public":                 *rsa_public,
+		"rsa_private":                *rsa_private,
+		"ecc_public":                 *ecc_public,
+		"ecc_private":                *ecc_private,
+		"decrypt":                    *decrypt,
+		"sym_cipher":                 *sym_cipher,
+		"generate_keys":              *generate_keys,
+		"killprocs":                  *killprocs,                  // TODO
+		"vss":                        *vss,                        // TODO
+		"encryption_percent":         *encryption_percent,         // TODO
+		"threshold_auto_fullencrypt": *threshold_auto_fullencrypt, // TODO
 	}
 
 	return arguments, nil
