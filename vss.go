511e3e4205cb7842b01e8f37c67224acd7c9a086
panscan <joeavanzato@gmail.com>
2025-04-20 10:03:39 -0400

    Adding capability to enumerate and remove VSS for local drives

--- vss.go
+++ vss.go
@@ -1,7 +1,39 @@
 package main
 
-func RemoveShadowCopies(isRemoteDevice bool) error {
-	// TODO
+import (
+	"fmt"
+	"github.com/joeavanzato/impact/drives"
+	"github.com/mxk/go-vss"
+)
 
+func RemoveShadowCopies(isRemoteDevice bool) error {
+	if isRemoteDevice {
+		// We won't do this remotely, only if running locally
+		return nil
+	}
+	localDrives, err := drives.GetLocalDrives()
+	if err != nil {
+		return err
+	}
+	for _, drive := range localDrives {
+		if drive.IsRemovable == true {
+			continue
+		}
+		copies, err := vss.List(drive.Name)
+		if err != nil {
+			continue
+		}
+		if len(copies) == 0 {
+			continue
+		}
+		for _, c := range copies {
+			printFormattedMessage(fmt.Sprintf("Deleting shadow copy: %s", c.ID), INFO)
+			err = vss.Remove(c.ID)
+			if err != nil {
+				printFormattedMessage(fmt.Sprintf("Error deleting shadow copy: %s", err.Error()), ERROR)
+			}
+		}
+
+	}
 	return nil
 }
