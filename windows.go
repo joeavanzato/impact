5db4718fc5f0538c6f8e287a0c0585b6046b7c2d
panscan <joeavanzato@gmail.com>
2025-04-20 10:04:00 -0400

    Helper imp for checking if we are reunning as admin - basic.

---
+++ windows.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"golang.org/x/sys/windows"
+	"log"
+)
+
+// https://github.com/golang/go/issues/28804
+func areWeAdmin() bool {
+	var sid *windows.SID
+	// Although this looks scary, it is directly copied from the
+	// official windows documentation. The Go API for this is a
+	// direct wrap around the official C++ API.
+	// See https://docs.microsoft.com/en-us/windows/desktop/api/securitybaseapi/nf-securitybaseapi-checktokenmembership
+	err := windows.AllocateAndInitializeSid(
+		&windows.SECURITY_NT_AUTHORITY,
+		2,
+		windows.SECURITY_BUILTIN_DOMAIN_RID,
+		windows.DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&sid)
+	if err != nil {
+		log.Fatalf("SID Error: %s", err)
+		return false
+	}
+	defer windows.FreeSid(sid)
+
+	// This appears to cast a null pointer so I'm not sure why this
+	// works, but this guy says it does and it Works for Me™:
+	// https://github.com/golang/go/issues/28804#issuecomment-438838144
+	token := windows.Token(0)
+
+	member, err := token.IsMember(sid)
+	if err != nil {
+		log.Fatalf("Token Membership Error: %s", err)
+		return false
+	}
+
+	// Also note that an admin is _not_ necessarily considered
+	// elevated.
+	// For elevation see https://github.com/mozey/run-as-admin
+	//fmt.Println("Elevated?", token.IsElevated())
+
+	//fmt.Println("Admin?", member)
+	return member
+}
