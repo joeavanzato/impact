07703e79b51cc1dfaa2b0dfbfba5ddae761a3896
panscan <joeavanzato@gmail.com>
2025-04-20 16:26:37 -0400

    Making script proper one-liner using ADSI searcher

--- ad.go
+++ ad.go
@@ -4,20 +4,7 @@ import "fmt"
 
 func writeEnabledADComputers(targetFile string) error {
 	// TODO - Add option to use native LDAP query instead of PowerShell
-	script := fmt.Sprintf(`
-$outputFile = "%s";
-$searcher = New-Object System.DirectoryServices.DirectorySearcher;
-$searcher.Filter = "(&(objectCategory=computer)(objectClass=computer)(!userAccountControl:1.2.840.113556.1.4.803:=2))";
-$searcher.PageSize = 100000;$searcher.PropertiesToLoad.Add("name") | Out-Null;
-$results = $searcher.FindAll();
-if (Test-Path $outputFile) {
-    Clear-Content $outputFile;
-};
-foreach ($computer in $results) {
-    $hostname = $computer.Properties["name"][0];
-    Add-Content -Path $outputFile -Value $hostname;
-};
-`, targetFile)
+	script := fmt.Sprintf(`$outputFile = '%s';$searcher = New-Object System.DirectoryServices.DirectorySearcher;$searcher.Filter = '(&(objectCategory=computer)(objectClass=computer)(!userAccountControl:1.2.840.113556.1.4.803:=2))';$searcher.PageSize = 100000;$searcher.PropertiesToLoad.Add('name') | Out-Null;$results = $searcher.FindAll();if (Test-Path $outputFile) {Clear-Content $outputFile;};foreach ($computer in $results) {$hostname = $computer.Properties['name'][0];Add-Content -Path $outputFile -Value $hostname;};`, targetFile)
 	// Execute the PowerShell script
 	err := runPowerShell(script)
 	if err != nil {
